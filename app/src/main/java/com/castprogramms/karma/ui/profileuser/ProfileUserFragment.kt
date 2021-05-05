15340dbffc0edb996762f508487f542553edf9f2
polina1441 <14042003dp@mail.ru>
2021-05-05 18:00:17 +0700

    profile

--- app/src/main/java/com/castprogramms/karma/ui/profileuser/ProfileUserFragment.kt
+++ app/src/main/java/com/castprogramms/karma/ui/profileuser/ProfileUserFragment.kt
@@ -1,23 +1,17 @@
 package com.castprogramms.karma.ui.profileuser
 
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import com.bumptech.glide.Glide
 import com.castprogramms.karma.R
 import com.castprogramms.karma.databinding.ProfileUserFragmentBinding
 import com.castprogramms.karma.network.Resource
 import com.castprogramms.karma.tools.Score
 import com.castprogramms.karma.tools.Service
-import com.castprogramms.karma.ui.adapters.MyServicesAdapter
-import com.castprogramms.karma.ui.adapters.ServiceSliderAdapter
 import com.castprogramms.karma.ui.adapters.ServicesAdapter
-import com.google.android.material.tabs.TabLayoutMediator
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class ProfileUserFragment : Fragment() {
@@ -25,7 +19,7 @@ class ProfileUserFragment : Fragment() {
     var idAuthor = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (arguments != null){
+        if (arguments != null) {
             idAuthor = arguments?.getString("id").toString()
         }
     }
@@ -34,14 +28,14 @@ class ProfileUserFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        val view =  inflater.inflate(R.layout.profile_user_fragment, container, false)
+        val view = inflater.inflate(R.layout.profile_user_fragment, container, false)
         val binding = ProfileUserFragmentBinding.bind(view)
-        val adapter = ServicesAdapter({}, {id, score ->  })
+        val adapter = ServicesAdapter({}, { id, score -> })
         binding.myRecServiceProfile.layoutManager = GridLayoutManager(requireContext(), 2)
         binding.myRecServiceProfile.adapter = adapter
 
-        profileUserViewModel.getUserServices(idAuthor).observe(viewLifecycleOwner){
-            when(it){
+        profileUserViewModel.getUserServices(idAuthor).observe(viewLifecycleOwner) {
+            when (it) {
                 is Resource.Error -> { }
                 is Resource.Loading -> { }
                 is Resource.Success -> {
@@ -50,10 +44,10 @@ class ProfileUserFragment : Fragment() {
             }
         }
 
-        profileUserViewModel.getUserData(idAuthor).observe(viewLifecycleOwner,{
-            when(it){
-                is Resource.Error -> {}
-                is Resource.Loading -> {}
+        profileUserViewModel.getUserData(idAuthor).observe(viewLifecycleOwner, {
+            when (it) {
+                is Resource.Error -> { }
+                is Resource.Loading -> { }
                 is Resource.Success -> {
                     val user = it.data?.second
                     if (user != null) {
@@ -64,6 +58,7 @@ class ProfileUserFragment : Fragment() {
         })
         return view
     }
+
     fun getListServices(list: List<Pair<String, Service>>): Pair<MutableList<Service>, MutableList<String>> {
         val mutableList = mutableListOf<Service>()
         val ids = mutableListOf<String>()
@@ -74,7 +69,7 @@ class ProfileUserFragment : Fragment() {
         return mutableList to ids
     }
 
-    fun countScores(scores: List<Score>): Int{
+    fun countScores(scores: List<Score>): Int {
         var sum = 0
         scores.forEach {
             sum += it.value
